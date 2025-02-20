5a093353239d7afb1e61907326f763aad14a5298
hrrsheen <h.r.r.sheen@gmail.com>
2025-02-20 12:56:28 +1000

    Cleaned up the day 6 code a little.

--- day06/main.go
+++ day06/main.go
@@ -28,11 +28,11 @@ type Guard struct {
  * Functions to query the grid.
  */
 
-func ToIndex(pos Vec, grid *Grid) int {
+func (grid *Grid) ToIndex(pos Vec) int {
 	return pos.y*grid.width + pos.x
 }
 
-func InBounds(pos Vec, grid *Grid) bool {
+func (grid *Grid) InBounds(pos Vec) bool {
 	return pos.x >= 0 && pos.x < grid.width && pos.y >= 0 && pos.y < grid.height
 }
 
@@ -40,12 +40,12 @@ func InBounds(pos Vec, grid *Grid) bool {
  * Functions to manipulate vectors.
  */
 
-func AddVec(a Vec, b Vec) Vec {
-	return Vec{a.x + b.x, a.y + b.y}
+func (vector Vec) Add(other Vec) Vec {
+	return Vec{vector.x + other.x, vector.y + other.y}
 }
 
-func EqualVec(a Vec, b Vec) bool {
-	return a.x == b.x && a.y == b.y
+func (vector Vec) Equal(other Vec) bool {
+	return vector.x == other.x && vector.y == other.y
 }
 
 /**
@@ -53,12 +53,12 @@ func EqualVec(a Vec, b Vec) bool {
  */
 
 func AvoidObstruction(guard *Guard, grid *Grid) bool {
-	stepAhead := AddVec(guard.pos, guard.dir)
-	if !InBounds(stepAhead, grid) {
+	stepAhead := guard.pos.Add(guard.dir)
+	if !grid.InBounds(stepAhead) {
 		return false
 	}
 
-	if grid.contents[ToIndex(stepAhead, grid)] == '#' {
+	if grid.contents[grid.ToIndex(stepAhead)] == '#' {
 		// Rotate the guard 90 degrees CW if an obstruction is directly ahead.
 		guard.dir = Vec{-guard.dir.y, guard.dir.x}
 		return true
@@ -71,24 +71,24 @@ func Step(guard *Guard, grid *Grid) bool {
 	for AvoidObstruction(guard, grid) {
 	}
 
-	guard.pos = AddVec(guard.pos, guard.dir)
-	return InBounds(guard.pos, grid)
+	guard.pos = guard.pos.Add(guard.dir)
+	return grid.InBounds(guard.pos)
 }
 
 func TestLoop(newObstruction Vec, guard *Guard, grid *Grid) bool {
-	if !InBounds(newObstruction, grid) {
+	if !grid.InBounds(newObstruction) {
 		return false
 	}
 
 	virtualGuard := *guard           // The simulated guard
 	pathHistory := make(map[Vec]Vec) // Records the position the guard was last facing at each position.
 
-	obstructionIndex := ToIndex(newObstruction, grid)
+	obstructionIndex := grid.ToIndex(newObstruction)
 	looping := false
 	// Place the obstruction for the similation.
 	grid.contents[obstructionIndex] = '#'
 	for Step(&virtualGuard, grid) {
-		if EqualVec(pathHistory[virtualGuard.pos], virtualGuard.dir) {
+		if pathHistory[virtualGuard.pos].Equal(virtualGuard.dir) {
 			// Loops are achieved when the guard reaches any given point and faces the same direction as the
 			// last time it was at that point.
 			looping = true
@@ -107,13 +107,13 @@ func WalkPatrol(guard *Guard, grid *Grid) (int, int) {
 	coveredTiles := make(map[Vec]bool) // A set of all tiles that the guard has visited.
 
 	loopsFormed := 0
-	for InBounds(guard.pos, grid) {
+	for grid.InBounds(guard.pos) {
 		coveredTiles[guard.pos] = true
 		// Turn untill the path ahead of the guard is empty.
 		for AvoidObstruction(guard, grid) {
 		}
 
-		nextPosition := AddVec(guard.dir, guard.pos)
+		nextPosition := guard.dir.Add(guard.pos)
 		// Place an obstruction in front of the guard and simulate the new path
 		// to determine if a loop forms.
 		// We don't do this for tiles that the guard has already patroled though. They might notice!
